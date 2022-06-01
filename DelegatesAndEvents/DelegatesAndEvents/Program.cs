511180ae0309bb74065b711321a5c7710c5fa51c
Reshma2310 <dreshma253@gmail.com>
2022-06-01 19:58:33 +0500

    [Reshma] - Executed Demo Program of Delegates

---
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    public delegate int Operation(int x, int y);//Delegate Declaration
+    class Program
+    {
+        static int Addition(int a, int b)//Method that Passes as an argument(same as Delegate)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate");
+            Operation obj = new Operation(Addition);//Delegate Installation
+            Console.WriteLine("Addition is : {0}", obj(25, 67));
+            Console.WriteLine();
+        }
+    }
+}
